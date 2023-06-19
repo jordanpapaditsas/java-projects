f2f1294ef73acfa2af5a42833d8795a97e2ff9a7
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-06-19 09:43:23 +0300

    Added Java doc and comments in TicTacToeApp

--- src/tic_tac_toe_app/TicTacToe.java
+++ src/tic_tac_toe_app/TicTacToe.java
@@ -2,6 +2,13 @@ package tic_tac_toe_app;
 
 import java.util.Scanner;
 
+/**
+ * Tic-tac-toe is a game for two players who take turns marking the spaces
+ * in a three-by-three grid with X or O.
+ * The player who succeeds in placing three of their marks in a
+ * horizontal, vertical, or diagonal row is the winner.
+ *
+ */
 public class TicTacToe {
 
     public static int[][] board;
@@ -18,11 +25,13 @@ public class TicTacToe {
         while (true) {
             showBoard();
 
+            // Getting player's move first by row, then by column.
             row = getPlayerMove("Move (row) (0-" + (BOARD_SIZE - 1) +
                     "): " + "Player" + " " + currentPlayer + "\n");
             col = getPlayerMove("Move (column) (0-" + (BOARD_SIZE - 1)
                     + "): " + "Player" + " " + currentPlayer + "\n");
 
+            // Checking if the given move is valid or not.
             if (!isValidMove(row, col)) {
                 System.out.println("Move not valid. Set a valid move please: ");
                 continue;
@@ -30,12 +39,14 @@ public class TicTacToe {
 
             makeMove(row, col);
 
+            // Checks with method if the player won the game.
             if (checkIfWin()) {
                 showBoard();
                 System.out.println("Player " + currentPlayer + " won the game!");
                 break;
             }
 
+            // Checks if the board is full, if the board is full then the game is a tie.
             if (isBoardFull()) {
                 showBoard();
                 System.out.println("Draw!");
@@ -51,6 +62,7 @@ public class TicTacToe {
         currentPlayer = PLAYER1;
     }
 
+    // Shows board and set Players symbols.
     public static void showBoard() {
         System.out.println();
         for (int i = 0; i < BOARD_SIZE; i++) {
@@ -69,20 +81,24 @@ public class TicTacToe {
         }
     }
 
+    // Reads players move.
     public static int getPlayerMove(String message) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(message);
         return scanner.nextInt();
     }
 
+    // Method for move validation.
     public static boolean isValidMove(int row, int col) {
         return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE && board[row][col] == 0;
     }
 
+    // Allows player to make a move by choosing row and column.
     public static void makeMove(int row, int col) {
         board[row][col] = currentPlayer;
     }
 
+    // Checks which player is playing currently.
     public static void changePlayer() {
         if (currentPlayer == PLAYER1) {
             currentPlayer = PLAYER2;
@@ -91,6 +107,7 @@ public class TicTacToe {
         }
     }
 
+    // Method for searching and checking if the board is full and players out of moves.
     public static boolean isBoardFull() {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
@@ -102,6 +119,8 @@ public class TicTacToe {
         return true;
     }
 
+
+    // Iterates each combination to check if a player won by making a 3 line combo.
     public static boolean checkIfWin() {
         for (int i = 0; i < BOARD_SIZE; i++) {
             if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
